e6c473c3d2336bd82621b9d30d25b7e67970dbcf
DevJashim <developerjashim@gmail.com>
2021-08-21 17:29:27 +0600

    06-Tabs app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 // ==App.js========================================
 
-
-
+import TabApp from "./components/TabApp";
 import "./sass/main.scss";
 
 const App = () => {
   return (
     <>
-     
+     <TabApp/>
     </>
   );
 };
---
+++ src/components/TabApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const url = "https://course-api.com/react-tabs-project";
+const TabApp = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default TabApp
