02b534b57b9ca3c500606416b1e4680a0d8358b2
DevJashim <developerjashim@gmail.com>
2021-09-15 07:58:19 +0600

    Final

--- src/components/TabApp.js
+++ src/components/TabApp.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { FaAngleDoubleRight } from "react-icons/fa";
+
 const url = "https://course-api.com/react-tabs-project";
 
 const TabApp = () => {
